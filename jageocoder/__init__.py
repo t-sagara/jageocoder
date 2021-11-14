a2e1bde452642f11adfc99c64d57ec8773a2f4bd
Takeshi Sagara <sagara@info-proto.com>
2021-11-14 14:22:56 +0900

    Add module docstring.

--- jageocoder/__init__.py
+++ jageocoder/__init__.py
@@ -1,5 +1,22 @@
 """
-jageocoder is a Python module for the Japanese-address geocoding.
+A Python module for Japanese-address geocoding.
+
+
+Note
+----
+Before using this module, install address-dictionary from the Web
+as follows:
+
+    $ python -m jageocoder install-dictionary
+
+Example
+-------
+You can get the latitude and longitude from a Japanese address by
+running the following steps.
+
+    >>> import jageocoder
+    >>> jageocoder.init()
+    >>> jageocoder.searchNode('<Japanese-address>')
 """
 
 __version__ = '0.3.0rc3'  # The package version
