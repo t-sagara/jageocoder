8ddfaefa7a49ee3e67e4edc3b43b1a49ea020f56
Takeshi Sagara <sagara@info-proto.com>
2022-03-13 16:26:21 +0900

    Remove 'url_prefix' implementation from flask-demo, since it shoud be handled by WSGI server.

--- flask-demo/app.py
+++ flask-demo/app.py
@@ -9,31 +9,22 @@ jageocoder.init()
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
-url_prefix = os.environ.get('URL_PREFIX', '')
-inner_url_prefix = os.environ.get('INNER_URL_PREFIX', '')
-if url_prefix != "" and url_prefix[-1] == "/":
-    url_prefix = url_prefix[:-1]
-if inner_url_prefix != "" and inner_url_prefix[-1] == "/":
-    inner_url_prefix = inner_url_prefix[:-1]
-
-
-@app.route(inner_url_prefix + "/")
+@app.route("/")
 def index():
     query = request.args.get('q') or ''
     skip_aza = request.args.get('skip_aza') or 'auto'
     return render_template(
         'index.html',
-        url_prefix=url_prefix,
         skip_aza=skip_aza,
         q=query, result=None)
 
 
-@app.route(inner_url_prefix + "/webapi")
+@app.route("/webapi")
 def webapi():
     return render_template('webapi.html')
 
 
-@app.route(inner_url_prefix + "/search", methods=['POST', 'GET'])
+@app.route("/search", methods=['POST', 'GET'])
 def search():
     query = request.args.get('q')
     skip_aza = request.args.get('skip_aza') or 'auto'
@@ -46,12 +37,11 @@ def search():
 
     return render_template(
         'index.html',
-        url_prefix=url_prefix,
         skip_aza=skip_aza,
         q=query, results=results)
 
 
-@app.route(inner_url_prefix + "/node/<id>", methods=['POST', 'GET'])
+@app.route("/node/<id>", methods=['POST', 'GET'])
 def show_node(id):
     node = jageocoder.get_module_tree().get_node_by_id(id)
     query = request.args.get('q')
@@ -59,13 +49,12 @@ def show_node(id):
 
     return render_template(
         'node.html',
-        url_prefix=url_prefix,
         skip_aza=skip_aza,
         q=query,
         node=node)
 
 
-@app.route(inner_url_prefix + "/geocode", methods=['POST', 'GET'])
+@app.route("/geocode", methods=['POST', 'GET'])
 @cross_origin()
 def geocode():
     if request.method == 'GET':
@@ -85,7 +74,7 @@ def geocode():
     return jsonify([x.to_dict() for x in results]), 200
 
 
-@app.route(inner_url_prefix + "/rgeocode", methods=['POST', 'GET'])
+@app.route("/rgeocode", methods=['POST', 'GET'])
 @cross_origin()
 def reverse_geocode():
     if request.method == 'GET':
---
+++ flask-demo/run.bat
@@ -0,0 +1,3 @@
+set FLASK_APP=app
+set FLASK_ENV=development
+flask run --host=0.0.0.0
