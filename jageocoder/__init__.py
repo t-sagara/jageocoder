d14082ff7fabefc149517c272ed363a8cffb23b0
Takeshi Sagara <sagara@info-proto.com>
2022-05-23 08:59:32 +0900

    v1.3.0

--- jageocoder/__init__.py
+++ jageocoder/__init__.py
@@ -19,7 +19,7 @@ running the following steps.
     >>> jageocoder.searchNode('<Japanese-address>')
 """
 
-__version__ = '1.3.0dev1'  # The package version
+__version__ = '1.3.0'  # The package version
 __dictionary_version__ = '20220519'  # Compatible dictionary version
 __author__ = 'Takeshi Sagara <sagara@info-proto.com>'
 
