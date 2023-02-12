a6f0d797e3fa7a8d6a91d5418ca787c5ada2767c
Takeshi Sagara <sagara@info-proto.com>
2023-02-12 15:56:22 +0900

    v1.3.2

--- jageocoder/base.py
+++ jageocoder/base.py
@@ -1,7 +1,18 @@
 import sqlalchemy
+from sqlalchemy.orm import registry
+
+
+mapper_registry = registry()
+
+
+Base = mapper_registry.generate_base()
+
+""" 1.4
+import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+"""
 
 
 def get_session(obj: Base):
